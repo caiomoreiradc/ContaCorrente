f6b742957c541e8001dbbaaa8a48b43797fa064c
caiomoreiradc <caiomoreiracarvalhoo@outlook.com>
2023-04-11 00:18:26 -0300

    Removed some stuff

--- ContaCorrente/ContaCorrente.cs
+++ ContaCorrente/ContaCorrente.cs
@@ -44,59 +44,59 @@ namespace ContaCorrente
         }
         public void SacarValor(int quantia)
         {
-            if(quantia < this.limite + this.saldo)
+            if(quantia < limite + saldo)
             {
-                this.saldo -= quantia;
+                saldo -= quantia;
 
                 Movimentacao mx = new Movimentacao();
                 mx.valor = quantia;
                 mx.tipo = "DÉBITO";
                 mx.descricao = $"Saque de R${quantia}.";
-                this.movimentacao[PosicaoVazia()] = mx;
+                movimentacao[PosicaoVazia()] = mx;
             }
         }        
         public void DepositarValor(int quantia)
         {
-            this.saldo += quantia;
+            saldo += quantia;
             
             Movimentacao mx = new Movimentacao();
             mx.valor = quantia;
             mx.tipo = "CRÉDITO";
             mx.descricao = $"Depósito de R${quantia}.";
-            this.movimentacao[PosicaoVazia()] = mx;
+            movimentacao[PosicaoVazia()] = mx;
 
         }
         public void VerSaldo(int saldo)
         {
-            Console.WriteLine($"Seu saldo é:R${this.saldo}");
+            Console.WriteLine($"Seu saldo é:R${saldo}");
         }
         public void VerExtrato()
         {
-            Console.Title = $"Banco do Caio - Extrato da Conta {this.numero}";
+            Console.Title = $"Banco do Caio - Extrato da Conta {numero}";
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine($"------------------------------");
-            Console.WriteLine($"           CONTA {this.numero}           ");
+            Console.WriteLine($"           CONTA {numero}           ");
             Console.WriteLine($"------------------------------");
-            Console.WriteLine($"Número: {this.numero}");
-            Console.WriteLine($"Saldo: R${this.saldo}");
-            Console.WriteLine($"Limite: R${this.limite}");
-            Console.WriteLine($"É conta especial: {this.MensagemEspecial()}");
+            Console.WriteLine($"Número: {numero}");
+            Console.WriteLine($"Saldo: R${saldo}");
+            Console.WriteLine($"Limite: R${limite}");
+            Console.WriteLine($"É conta especial: {MensagemEspecial()}");
             Console.WriteLine($"------------------------------");
             Console.WriteLine($"        MOVIMENTAÇÕES        ");
             Console.WriteLine($"------------------------------");
             Console.ResetColor();
             for (int i = 0; i < movimentacao.Length; i++) 
             { 
-                if (this.movimentacao[i] != null)
+                if (movimentacao[i] != null)
                 {
-                    this.movimentacao[i].ShowDescricao();
+                    movimentacao[i].ShowDescricao();
                 }
             }
             Console.WriteLine($"------------------------------", Console.ForegroundColor = ConsoleColor.Cyan);
         }
         public void FazerTransferencia(ContaCorrente conta2, int quantia)
         {
-            this.SacarValor(quantia);
+            SacarValor(quantia);
             conta2.DepositarValor(quantia);
         }
     }
